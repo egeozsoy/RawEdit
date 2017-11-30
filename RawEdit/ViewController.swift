8ec9cae3ca0fb07801c02d963f82ab63a1e3ae02
egeozsoy <egeozsoy97@gmail.com>
2017-11-30 17:42:49 +0100

    RawEdit

--- RawEdit/ViewController.swift
+++ RawEdit/ViewController.swift
@@ -7,19 +7,93 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import Photos
+class ViewController: UIViewController, UIImagePickerControllerDelegate , UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageViewer: UIImageView!
+    @IBOutlet weak var photoButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var exposureSlider: UISlider!
+    var rawURL : URL?
+    var rawAsCIImage : CIImage?
+    var rawImage: CIFilter?
+    
+    lazy var contextForSaving:CIContext = CIContext(options:
+        [kCIContextCacheIntermediates : false ,
+         kCIContextPriorityRequestLow : true ])
+    
+    @IBAction func selectPhoto(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    //    methods for the picker
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("canceled")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let rawURL = info["UIImagePickerControllerImageURL"] as? URL{
+            print(rawURL)
+            self.rawURL = rawURL
+            guard let rawAsCIImage = getAdjustedRaw() else {return}
+            self.rawAsCIImage = rawAsCIImage
+            let rawAsUIImage = UIImage(ciImage: rawAsCIImage)
+            print(rawAsUIImage)
+            imageViewer.image = rawAsUIImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func exposureChanged(_ sender: Any) {
+        guard let rawAsCIImage = getAdjustedRaw() else {return}
+        self.rawAsCIImage = rawAsCIImage
+        let rawAsUIImage = UIImage(ciImage: rawAsCIImage)
+        imageViewer.image = rawAsUIImage
+    }
+    
+    func getAdjustedRaw() -> CIImage?{
+    
+        guard let shadowHighlight = CIFilter(name: "CIHighlightShadowAdjust") else{return nil}
+        //open raw to edit
+        if self.rawImage == nil {
+            self.rawImage = CIFilter(imageURL: rawURL!, options: nil)
+        }
+        
+        guard let rawImage = self.rawImage else {return nil}
+        let exValue = exposureSlider.value
+        rawImage.setValue(exValue, forKey: kCIInputEVKey)
+        rawImage.setValue(2, forKey: kCIInputBoostShadowAmountKey)
+        shadowHighlight.setValue(rawImage.outputImage, forKey: kCIInputImageKey)
+        shadowHighlight.setValue(1, forKey: "inputHighlightAmount")
+        shadowHighlight.setValue(0.3 , forKey: "inputShadowAmount")
+        rawImage.setValue(shadowHighlight, forKey: kCIInputLinearSpaceFilter)
+    
+        return rawImage.outputImage
+    }
+    func createCGIImage(from rawImage: CIImage ) -> CGImage? {
+        return contextForSaving.createCGImage(rawImage, from: rawImage.extent, format: kCIFormatRGBAh, colorSpace: CGColorSpace(name:CGColorSpace.extendedLinearSRGB), deferred: true)
+    }
+    
+    @IBAction  func save(_ sender: Any) {
+        PHPhotoLibrary.shared().performChanges( {
+            let creationRequet = PHAssetCreationRequest.forAsset()
+            let creationOptions = PHAssetResourceCreationOptions()
+            creationOptions.shouldMoveFile = true
+            let cs = CGColorSpace(name: CGColorSpace.displayP3)!
+            let jpegPhotoData =  self.contextForSaving.jpegRepresentation(of: self.rawAsCIImage!, colorSpace: cs, options: [kCGImageDestinationLossyCompressionQuality : 1.0])
+            
+            creationRequet.addResource(with: .photo, data: jpegPhotoData!, options: creationOptions)
+        }, completionHandler: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        dont get values everytime
+        exposureSlider.isContinuous = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ RawEdit/futureReference.swift
@@ -0,0 +1,43 @@
+//
+//  futureReference.swift
+//  RawEdit
+//
+//  Created by Ege on 29.11.17.
+//  Copyright © 2017 Ege. All rights reserved.
+//
+
+    /*
+import Foundation
+
+
+changeColor () {
+        for basic color adjustments
+        guard let colorControls = CIFilter(name: "CIColorControls") else{return nil}
+       shadow highlight control from apple
+        doesnt really work with linearspace filter
+        guard let toneCurve = CIFilter(name: "CIToneCurve") else{return nil}
+
+ colorControls.setValue(rawImage.outputImage, forKey: kCIInputImageKey)
+ colorControls.setValue(1.2, forKey: kCIInputSaturationKey)
+ colorControls.setValue(0.0, forKey: kCIInputBrightnessKey)
+ colorControls.setValue(1, forKey: kCIInputContrastKey)
+ 
+ let vector0 = CIVector(x: 0.47, y: 0.0)
+ let vector1 = CIVector(x: 0.997, y: 1)
+ let vector2 = CIVector(x: 0.998, y: 1)
+ let vector3 = CIVector(x: 0.999, y: 1)
+ let vector4 = CIVector(x: 1, y: 1)
+ 
+ toneCurve.setValue(rawImage.outputImage, forKey: kCIInputImageKey)
+ toneCurve.setValue(vector0, forKey: "inputPoint0")
+ toneCurve.setValue(vector1, forKey: "inputPoint1")
+ toneCurve.setValue(vector2, forKey: "inputPoint2")
+ toneCurve.setValue(vector3, forKey: "inputPoint3")
+ toneCurve.setValue(vector4, forKey: "inputPoint4")
+ 
+ if let output = toneCurve.value(forKey: kCIOutputImageKey) as? CIImage{
+ return output
+ }
+}
+ */
+
